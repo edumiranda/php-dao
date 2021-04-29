897e4d6e156133ba4d439de36b2888cbfd7ba36a
eduardo <dumiranda@gmail.com>
2021-04-29 14:45:45 -0300

    demais metodos crud

--- class/Usuario.php
+++ class/Usuario.php
@@ -41,6 +41,13 @@ class Usuario{
         $this->dtcadastro=$value;
     }
 
+    public function setData($row){
+        $this->setIdusuario($row["id_usuario"]);
+        $this->setSenha($row["ds_senha"]);
+        $this->setLogin($row["ds_login"]);
+        $this->setDtcadastro(new DateTime($row["dt_cadastro"]));
+    }
+
     public function loadById($id){
         $sql = new Sql();
 
@@ -49,14 +56,76 @@ class Usuario{
         ));
 
         if (count($results)>0){
-            $row = $results[0];
+            $this->setData($results[0]);
 
-            $this->setIdusuario($row["id_usuario"]);
-            $this->setSenha($row["ds_senha"]);
-            $this->setLogin($row["ds_login"]);
-            $this->setDtcadastro(new DateTime($row["dt_cadastro"]));
         }
     }
+
+    public static function getList(){
+        $sql = new Sql();
+
+        return $sql->select("select * from tb_usuarios order by ds_login");
+
+    }
+
+    public static function search($deslogin){
+        $sql = new Sql();
+
+        return $sql->select("select * from tb_usuarios where ds_login like :SEARCH order by ds_login", array(
+            ":SEARCH"=>"%".$deslogin."%"
+        ));
+
+    }
+
+    public function __construct($deslogin="", $dessenha="")
+    {
+       $this->setLogin($deslogin);
+       $this->setSenha($dessenha); 
+    }
+
+    public function insert(){
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuario_insert(:LOGIN, :SENHA)", array(
+            ':LOGIN'=>$this->getLogin(),
+            ':SENHA'=>$this->getSenha()
+        ));
+
+        if (count($results)>0){
+            $this->setData($results[0]);
+        }else{
+            throw new Exception("Insert sem sucesso!");
+        }
+
+    }
+
+    public function update($deslogin, $dessenha){
+        $sql = new Sql();
+        $this->setLogin($deslogin);
+        $this->setSenha($dessenha);
+
+        $sql->executeQuery("UPDATE tb_usuarios SET ds_login = :LOGIN, ds_senha = :SENHA where id_usuario=:ID", array(
+            ':LOGIN'=>$deslogin,
+            ':SENHA'=>$dessenha,
+            ':ID'=>$this->getIdusuario()
+        ));
+    }
+
+    public function delete(){
+        $sql = new Sql();
+        
+        $sql->executeQuery("DELETE tb_usuarios where id_usuario=:ID", array(
+            ':ID'=>$this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setLogin("");
+        $this->setSenha("");
+        $this->setDtcadastro(new DateTime());
+        
+    }
+
+    
 
     public function __toString(){
         return json_encode(array(
@@ -65,6 +134,21 @@ class Usuario{
             "senha"=>$this->getSenha(),
             "dt cadastro"=>$this->getDtcadastro()->format("d/m/y H:i:s")
         ));
+    }
+
+    public function login($login, $senha){
+        $sql = new Sql();
+
+        $results = $sql->select("select * from tb_usuarios where ds_login=:login and ds_senha=:senha",array(
+            ":login"=>$login,
+            ":senha"=>$senha
+        ));
+
+        if (count($results)>0){
+            $this->setData($results[0]);
+        }else{
+            throw new Exception("Dados de autenticação invalidos");
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -10,8 +10,47 @@
     exit
     */
     
+    /*
     $usuario = new Usuario();
     $usuario->loadById(1);
+    echo $usuario;
+    */
+
+    /*
+    $lista = Usuario::getList();
+    echo json_encode($lista);
+    */
+
+    
+    /*
+    $lista = Usuario::search("user");
+    echo json_encode($lista);
+    */
+
+    /*
+    $usuario = new Usuario();
+    $usuario->login("user","12345");
+    echo $usuario;
+    */
+
+    /*
+    $usuario = new Usuario("aluno","1234_2");
+    $usuario->insert();
+    echo $usuario;
+    */
+
+    /*
+    $usuario = new Usuario();
+    $usuario->loadById(8);
+    echo $usuario;
+    $usuario->update("professor","654321");
+    echo $usuario;
+    */
+
+    $usuario = new Usuario();
+    $usuario->loadById(8);
+    echo $usuario;
+    $usuario->delete();
     echo $usuario;
 
 ?>
